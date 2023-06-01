0ae6730d5284c97d77cc465742cadf7d22621fed
bogdanpetrovic <bogipetrovic069@gmail.com>
2023-06-01 23:46:37 +0200

    generisane komponente, dodati fajlovi (prazni su), ubacene slike

--- se325project/src/app/app.module.ts
+++ se325project/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DetailsComponent } from './components/details/details.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { RentedComponent } from './components/rented/rented.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    DetailsComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    SignupComponent,
+    WishlistComponent,
+    RentedComponent
   ],
   imports: [
     BrowserModule,

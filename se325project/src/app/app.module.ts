e4a612336ac0f256713d224f400fc7f5d631e1c8
bogdanpetrovic <bogipetrovic069@gmail.com>
2023-06-02 00:01:30 +0200

    obrisani import-i za material (ne koristimo)

--- se325project/src/app/app.module.ts
+++ se325project/src/app/app.module.ts
@@ -1,29 +1,22 @@
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { GaugeModule } from 'angular-gauge';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DetailsComponent } from './components/details/details.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { WishlistComponent } from './components/wishlist/wishlist.component';
-import { RentedComponent } from './components/rented/rented.component';
+// import { GaugeModule } from 'angular-gauge';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {DetailsComponent} from './components/details/details.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {AboutComponent} from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {WishlistComponent} from './components/wishlist/wishlist.component';
+import {RentedComponent} from './components/rented/rented.component';
 
 @NgModule({
   declarations: [
@@ -42,17 +35,14 @@ import { RentedComponent } from './components/rented/rented.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GaugeModule.forRoot(),
+    // GaugeModule.forRoot(),
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatTabsModule,
-    MatIconModule,
     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}

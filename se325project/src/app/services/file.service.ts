66a33d895cfe2018d210eae824956ffc8fe8cf82
bogdanpetrovic <bogipetrovic069@gmail.com>
2023-06-02 21:09:31 +0200

    added methods in file service for wishlist

--- se325project/src/app/services/file.service.ts
+++ se325project/src/app/services/file.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Car} from "../models";
+import {Car, User} from "../models";
 import carData from 'src/app/cars.json'
 import {HttpClient} from "@angular/common/http";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,27 @@ export class FileService {
     return this.cars.find((car) => car.id === id);
   }
 
+  private getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.users);
+  }
 
+  addToWishlist(car: Car) {
+    this.wishlist.push(car);
+  }
+
+  getWishlist(): Observable<Car[]> {
+    return of(this.wishlist);
+  }
+
+  clearWishlist() {
+    this.wishlist = [];
+  }
+
+  removeFromWishlist(car: Car) {
+    this.wishlist = this.wishlist.filter((c) => c.id !== car.id);
+  }
+
+  isInWishlist(car: Car) {
+    return this.wishlist.some((c) => c.id === car.id);
+  }
 }

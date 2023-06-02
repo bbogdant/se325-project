fd2062425a350dc7b15b5a7536d3430f7c219c4d
djolean <djoler001@gmail.com>
2023-06-02 21:12:19 +0200

    Integrisan FileService u NavBar i dodate metode za Login i Logout

--- se325project/src/app/components/navbar/navbar.component.ts
+++ se325project/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { FileService } from '../../services/file.service';
+import { SessionService } from '../../services/session.service';
 import { User } from 'src/app/models';
 
 @Component({
@@ -12,8 +13,23 @@ export class NavbarComponent implements OnInit {
 
   user: User | undefined;
 
-  constructor(private router: Router) { }
+  constructor(private session: SessionService, private router: Router, private fileService: FileService) { }
 
   ngOnInit(): void {
+  }
+
+
+  isLoggedIn() {
+    if (this.session.getIsLoggedIn()) {
+      this.user = this.session.user;
+      return true;
+    }
+    return false;
+  }
+
+  logout() {
+    this.session.logout();
+    this.fileService.clearWishlist();
+    this.router.navigate(['/login']);
   }
 }

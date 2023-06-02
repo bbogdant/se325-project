6d83e1903592fc311797c1c461f8ddf7c328f4c9
bogdanpetrovic <bogipetrovic069@gmail.com>
2023-06-02 21:05:26 +0200

    wip on file.service

--- se325project/src/app/services/file.service.ts
+++ se325project/src/app/services/file.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {Car} from "../models";
+import carData from 'src/app/cars.json'
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
+  cars: Car[] = carData;
+  users = 'db.json';
 
-  constructor() { }
+  private wishlist: Car[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  getCar(id: number): Car | undefined {
+    return this.cars.find((car) => car.id === id);
+  }
 }

e363b87c5340f6bfc3aa411824170d5056382f4f
djolean <djoler001@gmail.com>
2023-06-02 21:25:02 +0200

    Footer added

--- se325project/src/app/components/footer/footer.component.ts
+++ se325project/src/app/components/footer/footer.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FileService } from 'src/app/services/file.service';
+import { SessionService } from 'src/app/services/session.service';
+import { User } from 'src/app/models';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  user: User | undefined;
+
+  constructor(private session: SessionService,private router: Router, private fileService: FileService) { }
+
+  ngOnInit(): void {
+  }
+
+  isLoggedIn() {
+    if (this.session.getIsLoggedIn()) {
+      this.user = this.session.user;
+      return true;
+    }
+    return false;
+  }
+
+  logout() {
+    this.session.logout();
+    this.fileService.clearWishlist();
+    this.router.navigate(['/login']);
+  }
 
 }

338178496b13503a4a4d7c1cc122125c10723bf6
miloseviccluka <milosevicc.luka@gmail.com>
2023-06-05 21:07:41 +0200

    added about component

--- se325project/src/app/components/about/about.component.ts
+++ se325project/src/app/components/about/about.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import teamData from 'src/app/team.json'
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Team } from 'src/app/models';
+
+
+
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  team: Team[] = teamData;
 }
